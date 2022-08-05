7fcc68f582f3534ccfd0defd34d99f10a5aa95ef
손정훈 <jhson2@humaxparcs.com>
2022-08-05 12:07:21 +0900

    test

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -7,7 +7,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 class DemoApplicationTests {
 
     @Test
-    void contextLoads() {
+    void firstTest() {
+        System.out.println("goods");
     }
 
 }
